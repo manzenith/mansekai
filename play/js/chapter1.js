941718037ed8a14344d4ffae1b2698756bc02936
manzenith <zenithadventourbandung@gmail.com>
2026-09-11 09:57:27 +0700

    Add files via upload

---
+++ play/js/chapter1.js
@@ -0,0 +1,90 @@
+const chapter1Script = [
+    {
+        speaker: "",
+        role: "",
+        location: "Kamar Firman",
+        text: "Kamar tidur Firman. Suasana siang yang sunyi, di mana ia tengah sibuk menatap layar laptop tanpa tujuan pasti.",
+        bg: "https://i.pinimg.com/736x/b3/3a/fd/b33afda2ad522a4b5ad7b1c328288adc.jpg",
+        image: "",
+        bgm: "stop" 
+    },
+    {
+        speaker: "Firman",
+        role: "Developer & Trader",
+        location: "Kamar Firman",
+        text: "Proyek freelance sepi, porto trading lagi merah... Hidup gini-gini amat ya. Main game mulu juga lama-lama jenuh.",
+        image: "../img/Firman_Hidayat/HD_Man.png",
+        bgm: "audio/nature.mp3" // Ubah ke file lagu lain, jangan pakai title_bgm.mp3 lagi di sini
+    },
+    {
+        speaker: "",
+        role: "",
+        location: "Kamar Firman",
+        text: "(Terdengar ketukan pintu, lalu pintu terbuka memperlihatkan Mamah yang membawa segelas teh hangat)",
+        image: ""
+    },
+    {
+        speaker: "Mamah",
+        role: "Ibu Firman",
+        location: "Kamar Firman",
+        text: "Firman, ini diminum dulu tehnya. Dari pagi ngadep layar terus, matanya nanti rusak.",
+        image: ""
+    },
+    {
+        speaker: "Firman",
+        role: "Developer & Trader",
+        location: "Kamar Firman",
+        text: "Makasih, Mah. Firman lagi pusing mikirin ide buat project baru nih, belum dapet yang klop.",
+        image: "../img/Firman_Hidayat/HD_Man.png"
+    },
+    {
+        speaker: "Mamah",
+        role: "Ibu Firman",
+        location: "Kamar Firman",
+        text: "Makanya jangan cuma rebahan di kamar. Coba nih lihat brosur yang Mamah bawa dari balai warga kemarin. Pelatihan sertifikasi Digital Marketing.",
+        image: ""
+    },
+    {
+        speaker: "Firman",
+        role: "Developer & Trader",
+        location: "Kamar Firman",
+        text: "Digital Marketing? Emangnya penting ya, Mah buat anak IT kayak Firman?",
+        image: "../img/Firman_Hidayat/HD_Man.png"
+    },
+    {
+        speaker: "Mamah",
+        role: "Ibu Firman",
+        location: "Kamar Firman",
+        text: "Penting banget! Jago bikin aplikasi atau web tapi kalau nggak bisa jualan atau memasarkannya ya sama aja bohong. Coba didaftar, mumpung pendaftarannya lagi gratis.",
+        image: ""
+    },
+    {
+        speaker: "Firman",
+        role: "Developer & Trader",
+        location: "Kamar Firman",
+        text: "Bener juga sih kata Mamah... Oke deh, nanti coba Firman pelajari persyaratannya.",
+        image: "../img/Firman_Hidayat/HD_Man.png"
+    },
+    {
+        speaker: "Mamah",
+        role: "Ibu Firman",
+        location: "Kamar Firman",
+        text: "Nah, gitu dong. Jangan mau kalah sama anak tetangga. Ya udah, Mamah mau lanjut beres-beres dulu.",
+        image: ""
+    },
+    {
+        speaker: "",
+        role: "",
+        location: "Kamar Firman",
+        text: "Selepas kepergian Mamah, Firman merenung sejenak lalu menatap serius ke arah layar laptopnya...",
+        image: ""
+    },
+    {
+        speaker: "Firman",
+        role: "Developer & Trader",
+        location: "Kamar Firman",
+        text: "Baiklah! Tantangan baru di dunia digital marketing sepertinya patut dicoba untuk memperluas skill.",
+        image: "../img/Firman_Hidayat/HD_Man.png",
+        bgm: "stop" 
+    }
+];
---
+++ play/js/chapter2.js
@@ -0,0 +1,89 @@
+const chapter2Script = [
+    {
+        speaker: "",
+        role: "",
+        location: "Fakultas Ekonomi & Bisnis",
+        text: "Keesokan harinya, suasana kampus tampak ramai. Firman berjalan menuju kantin area fakultas.",
+        bg: "https://i.pinimg.com/736x/b3/3a/fd/b33afda2ad522a4b5ad7b1c328288adc.jpg",
+        image: "",
+        bgm: "audio/campus- cheerful.mp3" // Ganti path audio BGM ceria kampus kamu
+    },
+    {
+        speaker: "Tania",
+        role: "Mahasiswa Seni Rupa UPI",
+        location: "Kantin Fakultas",
+        text: "Firman! Sini bro, kok dateng sendirian? Tadi keliatan serius banget mukanya.",
+        image: "../img/Titania_Felycia/HD_Titania.png"
+    },
+    {
+        speaker: "Firman",
+        role: "Developer & Trader",
+        location: "Kantin Fakultas",
+        text: "Eh, Tania. Lagi mikirin saran mamahku kemarin, suruh ambil course Digital Marketing. Kamu sendiri lagi ngapain di sini?",
+        image: "../img/Firman_Hidayat/HD_Man.png"
+    },
+    {
+        speaker: "Tania",
+        role: "Mahasiswa Seni Rupa UPI",
+        location: "Kantin Fakultas",
+        text: "Seriusan mau ambil Digital Marketing?! Wah, sumpah aku juga lagi nyari info pendaftaran course itu buat bantu jualan karya seni online-ku!",
+        image: "../img/Titania_Felycia/HD_Titania.png"
+    },
+    {
+        speaker: "Firman",
+        role: "Developer & Trader",
+        location: "Kantin Fakultas",
+        text: "Wah, kebetulan banget kalau gitu. Kita bisa barengan belajar strateginya.",
+        image: "../img/Firman_Hidayat/HD_Man.png"
+    },
+    {
+        speaker: "",
+        role: "",
+        location: "Kantin Fakultas",
+        text: "(Tiba-tiba seorang pria paruh baya berjas rapi menghampiri meja mereka)",
+        image: ""
+    },
+    {
+        speaker: "Dosen",
+        role: "",
+        location: "Kantin Fakultas",
+        text: "Hmm, membicarakan Digital Marketing ya? Topik yang sangat menarik di era modern seperti sekarang.",
+        image: "../img/dosen.png"
+    },
+    {
+        speaker: "Tania",
+        role: "Mahasiswa Seni Rupa UPI",
+        location: "Kantin Fakultas",
+        text: "Eh, Pak Dosen! Maaf Pak, kami tadi ngobrolin soal pelatihan strategi iklan.",
+        image: "../img/Titania_Felycia/HD_Titania.png"
+    },
+    {
+        speaker: "Dosen",
+        role: "",
+        location: "Kantin Fakultas",
+        text: "Tidak perlu meminta maaf. Justru bagus kalian antusias. Di kelas saya nanti, kalian tidak hanya belajar teori SEO di atas kertas, tapi langsung mempraktikkan riset pasar dan anggaran iklan riil.",
+        image: "../img/dosen.png"
+    },
+    {
+        speaker: "Firman",
+        role: "Developer & Trader",
+        location: "Kantin Fakultas",
+        text: "Pakai budget simulasi sungguhan dari kampus, Pak?",
+        image: "../img/Firman_Hidayat/HD_Man.png"
+    },
+    {
+        speaker: "Dosen",
+        role: "",
+        location: "Kantin Fakultas",
+        text: "Betul sekali. Siapkan mental dan kreativitas kalian. Sampai jumpa di ruang kuliah minggu depan!",
+        image: "../img/dosen.png"
+    },
+    {
+        speaker: "",
+        role: "",
+        location: "Koridor Kampus",
+        text: "Langkah awal telah ditetapkan. Petualangan mereka menaklukkan algoritma digital resmi dimulai di sini...",
+        image: "",
+        bgm: "stop" // Musik berhenti saat chapter 2 berakhir
+    }
+];
